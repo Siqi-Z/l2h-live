c435425d1c0a42b8323a6e5ece12c1082249c21a
project-pp <project-pp@users.noreply.github.com>
2018-03-25 17:53:23 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# l2h-live
+
+![screenshot.gif](./screenshot.gif)
+
+Live-preview for LaTeX document using [michael-brade/LaTeX.js](https://github.com/michael-brade/LaTeX.js)
+
+## Features
+
+- By using [LaTeX.js](https://github.com/michael-brade/LaTeX.js); stand-alone JavaScript LaTeX to HTML converter,
+  you could preview LaTeX document in VSCode without any external program.
+
+## Requirements
+
+Just install the extension into your VSCode.
+
+Note: At the moment this is BETA version and unpublished. I don't know how to install manually. See Development/Contribution.
+
+## Extension Settings
+
+## Known Issues
+
+- Ths is BETA version.
+- The quality of preview depends on the implementation of LaTeX.js.
+  If you contribute to LaTeX.js, also it would be help for this extension.
+
+## Release Notes
+
+## Development and Contribution
+
+You could try to develop this extension. After forking [Github repository](https://github.com/project-pp/l2h-live), :
+
+```
+$ git clone git@github.com:your-github-account/l2h-live.git
+$ cd l2h-live
+$ npm i -S -D
+$ code .
+```
+
+[Issues](https://github.com/project-pp/l2h-live/issues) or [PR](https://github.com/project-pp/l2h-live/pulls) are welcome.
+Please share your idea with us!
+
+## License
+
+[MIT](https://github.com/project-pp/l2h-live/blob/master/LICENSE)
---
+++ src/extension.ts
@@ -0,0 +1,119 @@
+'use strict';
+import * as vscode from 'vscode'
+import * as path from 'path'
+
+// load LaTeX.js
+// @ts-ignore
+global.window = require('svgdom')
+// @ts-ignore
+global.document = window.document
+const htmlGenerator = require('../node_modules/latex.js/dist/html-generator').HtmlGenerator
+const latexParser = require('../node_modules/latex.js/dist/latex-parser')
+
+export class LivePreviewProvider implements vscode.TextDocumentContentProvider  {
+    private context: vscode.ExtensionContext
+    private generator: any
+
+    public static readonly SCHEME = 'l2h-live-preview'
+
+    public constructor(context: vscode.ExtensionContext) {
+        this.context = context
+        this.generator = new htmlGenerator({ hyphenate: false })
+    }
+
+    // implements renderer
+    public provideTextDocumentContent(uri: vscode.Uri, token: vscode.CancellationToken) {
+        try {
+            // get source
+            const doc = vscode.workspace.textDocuments.find(doc => doc.uri.fsPath === uri.fsPath)
+            if (!doc) {
+                return null
+            }
+
+            // get DOM result
+            this.generator.reset()
+            const dom = latexParser.parse(doc.getText(), { generator: this.generator }).dom()
+
+            // fix CSS path
+            const links = Array.from(dom.head.querySelectorAll('link')) as ({ href: string })[]
+            links.forEach(link => link.href = vscode.Uri.file(
+                path.join(this.context.extensionPath, 'node_modules', 'latex.js', 'dist', link.href)).toString())
+
+            // to HTML source
+            return dom.outerHTML
+        } catch (err) {
+            return null
+        }
+    }
+
+    // implements update event
+    private _onDidChange = new vscode.EventEmitter<vscode.Uri>()
+    
+    public get onDidChange() {
+        return this._onDidChange.event
+    }
+
+    public update(uri: vscode.Uri): void {
+        this._onDidChange.fire(uri)
+    }
+}
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+    console.log('Congratulations, your extension "l2h-live" is now active!')
+
+    // check extension
+    const isTeX = (uri: vscode.Uri): boolean => [ 'tex', 'ltx', 'latex' ].some(ext => uri.fsPath.endsWith(ext))
+
+    // add command
+    context.subscriptions.push(vscode.commands.registerCommand('l2h-live.openPreview', () => {
+        // no active document selected
+        if (!vscode.window.activeTextEditor)
+        {
+            return
+        }
+
+        // no TeX document
+        if (!isTeX(vscode.window.activeTextEditor.document.uri)) {
+            return
+        }
+
+        // open
+        const uri = vscode.window.activeTextEditor.document.uri.with({scheme: LivePreviewProvider.SCHEME})
+        vscode.commands.executeCommand('vscode.previewHtml',
+            vscode.window.activeTextEditor.document.uri.with({scheme: LivePreviewProvider.SCHEME}),
+            vscode.ViewColumn.Two,
+            `Live preview for ${path.relative(vscode.workspace.rootPath || '', uri.fsPath)}`)
+    }))
+
+    // enable preview provider
+    const previewProvider = new LivePreviewProvider(context)
+    context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider(LivePreviewProvider.SCHEME, previewProvider))
+
+    // add content modification listeners
+    context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(e => {
+        if (typeof e === 'undefined') {
+            return
+        }
+        const active = vscode.window.activeTextEditor
+        if (active && isTeX(active.document.uri))
+        {
+            previewProvider.update(active.document.uri.with({ scheme: LivePreviewProvider.SCHEME }))
+        }
+    }))
+    context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(e => {
+        if (typeof e === 'undefined') {
+            return
+        }
+        const active = vscode.window.activeTextEditor
+        if (active && isTeX(active.document.uri))
+        {
+            previewProvider.update(active.document.uri.with({ scheme: LivePreviewProvider.SCHEME }))
+        }
+    }))
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
