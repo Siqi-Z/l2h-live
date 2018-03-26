978e70efe3ae5bd95bdd158ee91078092d5549d6
project-pp <project-pp@users.noreply.github.com>
2018-03-26 10:34:57 +0900

    clean source

--- src/extension.ts
+++ src/extension.ts
@@ -12,22 +12,29 @@ const htmlGenerator = require('../node_modules/latex.js/dist/html-generator').Ht
 const latexParser = require('../node_modules/latex.js/dist/latex-parser')
 
 export class LivePreviewProvider implements vscode.TextDocumentContentProvider  {
-    private context: vscode.ExtensionContext
-    private generator: any
-
     public static readonly SCHEME = 'l2h-live-preview'
+
+    private context: vscode.ExtensionContext
+    private _onDidChange: vscode.EventEmitter<vscode.Uri>
+    private generator: any
 
     public constructor(context: vscode.ExtensionContext) {
         this.context = context
+        this._onDidChange = new vscode.EventEmitter<vscode.Uri>()
         this.generator = new htmlGenerator({
             hyphenate: false,
             styles: [
                 // include from correct path
+                vscode.Uri.file(path.join(this.context.extensionPath, 'node_modules', 'latex.js', 'dist', 'css/katex.css')).toString(),
                 vscode.Uri.file(path.join(this.context.extensionPath, 'node_modules', 'latex.js', 'dist', 'css/article.css')).toString(),
                 // local changes
                 vscode.Uri.file(path.join(this.context.extensionPath, 'resources/fix.css')).toString(),
             ]
         })
+    }
+
+    public get onDidChange() {
+        return this._onDidChange.event
     }
 
     // implements renderer
@@ -56,72 +63,47 @@ export class LivePreviewProvider implements vscode.TextDocumentContentProvider  
         }
     }
 
-    // implements update event
-    private _onDidChange = new vscode.EventEmitter<vscode.Uri>()
-    
-    public get onDidChange() {
-        return this._onDidChange.event
+    // open preview tab
+    public open(editor: vscode.TextEditor): void {
+        vscode.commands.executeCommand('vscode.previewHtml',
+            editor.document.uri.with({ scheme: LivePreviewProvider.SCHEME }),
+            vscode.ViewColumn.Two,
+            `Live preview for ${path.relative(vscode.workspace.rootPath || '', editor.document.uri.fsPath)}`)
     }
 
-    public update(uri: vscode.Uri): void {
-        this._onDidChange.fire(uri)
+    public update(editor: vscode.TextEditor): void {
+        this._onDidChange.fire(editor.document.uri.with({ scheme: LivePreviewProvider.SCHEME }))
     }
 }
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Congratulations, your extension "l2h-live" is now active!')
-
     // check extension
     const isTeX = (uri: vscode.Uri): boolean => [ 'tex', 'ltx', 'latex' ].some(ext => uri.fsPath.endsWith(ext))
-
-    // add command
-    context.subscriptions.push(vscode.commands.registerCommand('l2h-live.openPreview', () => {
-        // no active document selected
-        if (!vscode.window.activeTextEditor)
-        {
-            return
-        }
-
-        // no TeX document
-        if (!isTeX(vscode.window.activeTextEditor.document.uri)) {
-            return
-        }
-
-        // open
-        const uri = vscode.window.activeTextEditor.document.uri.with({scheme: LivePreviewProvider.SCHEME})
-        vscode.commands.executeCommand('vscode.previewHtml',
-            vscode.window.activeTextEditor.document.uri.with({scheme: LivePreviewProvider.SCHEME}),
-            vscode.ViewColumn.Two,
-            `Live preview for ${path.relative(vscode.workspace.rootPath || '', uri.fsPath)}`)
-    }))
 
     // enable preview provider
     const previewProvider = new LivePreviewProvider(context)
     context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider(LivePreviewProvider.SCHEME, previewProvider))
 
+    // add command
+    context.subscriptions.push(vscode.commands.registerCommand('l2h-live.openPreview', () => {
+        if (vscode.window.activeTextEditor && isTeX(vscode.window.activeTextEditor.document.uri)) {
+            previewProvider.open(vscode.window.activeTextEditor)
+        }
+    }))
+
     // add content modification listeners
-    context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(e => {
-        if (typeof e === 'undefined') {
-            return
-        }
-        const active = vscode.window.activeTextEditor
-        if (active && isTeX(active.document.uri))
+    const onDocumentChanged = () => {
+        if (vscode.window.activeTextEditor && isTeX(vscode.window.activeTextEditor.document.uri))
         {
-            previewProvider.update(active.document.uri.with({ scheme: LivePreviewProvider.SCHEME }))
+            previewProvider.update(vscode.window.activeTextEditor)
         }
-    }))
-    context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(e => {
-        if (typeof e === 'undefined') {
-            return
-        }
-        const active = vscode.window.activeTextEditor
-        if (active && isTeX(active.document.uri))
-        {
-            previewProvider.update(active.document.uri.with({ scheme: LivePreviewProvider.SCHEME }))
-        }
-    }))
+    }
+    context.subscriptions.push(
+        vscode.window.onDidChangeActiveTextEditor(e => onDocumentChanged()),
+        vscode.window.onDidChangeTextEditorSelection(e => onDocumentChanged())
+    )
 }
 
 // this method is called when your extension is deactivated
